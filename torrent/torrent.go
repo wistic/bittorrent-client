a7b84c4ad357eccf474cc977b5b7f5a7900fbc9c
wistic <prathameshkatkar11@gmail.com>
2021-03-05 20:00:05 +0530

    Completely parsed Torrent

--- torrent/torrent.go
+++ torrent/torrent.go
@@ -1,7 +1,8 @@
 package torrent
 
 import (
-	"fmt"
+	"crypto/sha1"
+	"errors"
 	"github.com/IncSW/go-bencode"
 )
 
@@ -15,19 +16,54 @@ type Torrent struct {
 	Name        string
 }
 
+// splitHashes splits the give array of bytes to SHA1 hashes
+func splitHashes(pieceArray []byte) ([][20]byte, error) {
+	if len(pieceArray)%20 != 0 {
+		err := errors.New("piece hash information is corrupt")
+		return nil, err
+	}
+	numHashes := len(pieceArray) / 20
+	hashes := make([][20]byte, numHashes)
+
+	for i := 0; i < numHashes; i++ {
+		copy(hashes[i][:], pieceArray[i*20:(i+1)*20])
+	}
+	return hashes, nil
+}
+
+// infoHash returns the 20 byte sha1 hash of the bencoded form of the info value
+func infoHash(info interface{}) ([20]byte, error) {
+	data, err := bencode.Marshal(info)
+	if err != nil {
+		return [20]byte{}, err
+	}
+	return sha1.Sum(data), nil
+}
+
 // Parse parses the torrent code
 func Parse(code []byte) (Torrent, error) {
 	data, err := bencode.Unmarshal(code)
 	if err != nil {
 		return Torrent{}, err
 	}
+
 	dataMap := data.(map[string]interface{})
 	infoMap := dataMap["info"].(map[string]interface{})
-	fmt.Println(infoMap)
+
+	hashes, err := splitHashes(infoMap["pieces"].([]byte))
+	if err != nil {
+		return Torrent{}, err
+	}
+
+	infoHash, err := infoHash(dataMap["info"])
+	if err != nil {
+		return Torrent{}, err
+	}
+
 	tor := Torrent{
 		Announce:    string(dataMap["announce"].([]byte)),
-		InfoHash:    [20]byte{},
-		PieceHashes: nil,
+		InfoHash:    infoHash,
+		PieceHashes: hashes,
 		PieceLength: infoMap["piece length"].(int64),
 		Length:      infoMap["length"].(int64),
 		Name:        string(infoMap["name"].([]byte)),
